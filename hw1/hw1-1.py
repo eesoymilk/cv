67bc6c8fa9aeabd2a44b3501b8d965344edbfcb5
eesoymilk <qiyoudaoyi@gmail.com>
2023-10-17 15:25:26 +0800

    update env

--- hw1/hw1-1.py
+++ hw1/hw1-1.py
@@ -1,27 +1,26 @@
-import cv2 as cv
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
+from PIL import Image
 from numpy.typing import NDArray
 
 
-def showImage(img: NDArray):
-    # Convert the image from BGR to RGB (as OpenCV loads in BGR and matplotlib displays in RGB)
-    img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-
-    plt.imshow(img_rgb)
+def showImage(img: Image.Image | NDArray):
+    plt.imshow(img)
     plt.axis('off')  # to hide axis values
     plt.show()
 
 
 def convert_to_gray(
-    img: NDArray, rgb_weights: tuple = (0.299, 0.587, 0.114)
-) -> NDArray:
-    b, g, r = img[:, :, 0], img[:, :, 1], img[:, :, 2]
+    img: Image.Image | NDArray, rgb_weights: tuple = (0.299, 0.587, 0.114)
+) -> Image.Image:
+    img_array = np.array(img)
+    r, g, b = img_array[:, :, 0], img_array[:, :, 1], img_array[:, :, 2]
     r_weight, g_weight, b_weight = rgb_weights
-    gray = (r_weight * r + g_weight * g + b_weight * b).astype(np.uint8)
-    return gray
+    gray_img_array = (r_weight * r + g_weight * g + b_weight * b).astype(
+        np.uint8
+    )
+    return Image.fromarray(gray_img_array, 'L')
 
 
 def histogram_equalization(img: NDArray) -> NDArray:
@@ -29,11 +28,9 @@ def histogram_equalization(img: NDArray) -> NDArray:
 
 
 def main():
-    img: NDArray = cv.imread('hw1-1.jpg')
-
+    img = Image.open('hw1-1.jpg')
     gray_img = convert_to_gray(img)
-
-    showImage(img)
+    showImage(gray_img)
 
 
 if __name__ == '__main__':
